4f3fe1476849191decd2917f38893f4d4f52f491
Riley Mathews <rileymathews80@gmail.com>
2023-12-28 09:37:18 -0600

    added nvim config and improved layout

---
+++ nvim/lua/rileymathews/theme.lua
@@ -0,0 +1,9 @@
+vim.cmd.colorscheme "catppuccin"
+require("lualine").setup {
+    options = {
+        icons_enabled = false,
+        theme = 'catppuccin',
+        component_separators = '|',
+        section_separators = '',
+    },
+}
--- setup.sh
+++ setup.sh
@@ -1,3 +1,9 @@
 #! /usr/bin/env sh
 
-ln -s `pwd`/.tmux.conf ~/.tmux.conf
+echo "removing existing links"
+rm -rf ~/.tmux.conf
+rm -rf ~/.config/nvim
+
+echo "adding symlinks"
+ln -s `pwd`/tmux/.tmux.conf ~/.tmux.conf
+ln -s `pwd`/nvim ~/.config/nvim
