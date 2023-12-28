56acf90d95305baf65c3e29e5cda70438ef8f9f6
Riley Mathews <rileymathews80@gmail.com>
2023-12-28 17:05:11 -0600

    added none ls formatting

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,16 +1,15 @@
 return {
 	-- Highlight, edit, and navigate code
-	'nvim-treesitter/nvim-treesitter',
+	"nvim-treesitter/nvim-treesitter",
 	dependencies = {
-		'nvim-treesitter/nvim-treesitter-textobjects',
+		"nvim-treesitter/nvim-treesitter-textobjects",
 	},
-	build = ':TSUpdate',
-    config = function()
-        require('nvim-treesitter.configs').setup {
-            ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'html', 'javascript', 'typescript', 'vimdoc', 'vim', 'bash' },
-
-            highlight = { enable = true },
-            indent = { enable = true },
-        }
-    end
+	build = ":TSUpdate",
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			auto_install = true,
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
 }
