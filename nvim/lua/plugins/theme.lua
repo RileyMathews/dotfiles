4b3b5ba08032b2706403e69e567ea154ec0e5c85
Riley Mathews <rileymathews80@gmail.com>
2023-12-29 17:46:30 -0600

    lualine config

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -11,10 +11,7 @@ return {
         'nvim-lualine/lualine.nvim',
         opts = {
             options = {
-                icons_enabled = false,
                 theme = 'catppuccin',
-                component_separators = '|',
-                section_separators = '',
             },
         }
     }
