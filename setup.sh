3eade713f4a73b55d81116d8c8a0a71e60d7a56b
Riley Mathews <rileymathews80@gmail.com>
2024-01-19 15:07:56 -0600

    added possibility for local only tmux layouts

--- setup.sh
+++ setup.sh
@@ -5,11 +5,11 @@ rm -rf ~/.tmux.conf
 rm -rf ~/.config/nvim
 rm -rf ~/.zshrc
 rm -rf ~/zshrc
-rm -rf ~/.tmuxifier/layouts
+find ~/.tmuxifier/layouts -type l -delete
 
 echo "adding symlinks"
 ln -s `pwd`/tmux/.tmux.conf ~/.tmux.conf
 ln -s `pwd`/nvim ~/.config/nvim
 ln -s `pwd`/zsh/.zshrc ~/.zshrc
 ln -s `pwd`/zsh/zshrc ~/zshrc
-ln -s `pwd`/tmuxifier/layouts ~/.tmuxifier/layouts
+ln -s `pwd`/tmuxifier/layouts/* ~/.tmuxifier/layouts
