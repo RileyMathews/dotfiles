c70819a677002255428b7044d6156684df1fa708
Riley Mathews <rileymathews80@gmail.com>
2024-01-22 14:08:52 -0600

    added pyenv to installation

--- install.sh
+++ install.sh
@@ -2,3 +2,4 @@
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
 git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
