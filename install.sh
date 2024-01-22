26901422b697b5b1f4513947875e416bcc923806
Riley Mathews <rileymathews80@gmail.com>
2024-01-22 14:04:45 -0600

    added rbenv

--- install.sh
+++ install.sh
@@ -1,2 +1,4 @@
 #! /usr/bin/env zsh
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
