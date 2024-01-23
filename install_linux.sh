be78a1e69c88a0aabaa2f5b07d62760d2fdf2199
Riley Mathews <rileymathews80@gmail.com>
2024-01-23 17:56:11 +0000

    edited linux install

--- install_linux.sh
+++ install_linux.sh
@@ -1,7 +1,6 @@
 git clone https://github.com/neovim/neovim.git ~/neovim-src
 cd ~/neovim-src
 rm -rf ./build/
-rm -r build/  # clear the CMake cache
 make CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$HOME/neovim"
 make install
 export PATH="$HOME/neovim/bin:$PATH"
