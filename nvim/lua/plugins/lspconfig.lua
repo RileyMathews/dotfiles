08abb009989d5b2d8a5caebeb620064a9b32ee99
Riley Mathews <rmathews@eventbrite.com>
2024-01-25 13:31:12 -0600

    edited lsp hotkeys

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -18,10 +18,6 @@ return {
         --  This function gets run when an LSP connects to a particular buffer.
         local on_attach = function(_, bufnr)
             local nmap = function(keys, func, desc)
-                if desc then
-                    desc = 'LSP: ' .. desc
-                end
-
                 vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
             end
 
@@ -35,9 +31,8 @@ return {
             nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
             nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
 
-            -- See `:help K` for why this keymap
             nmap('<leader>pd', vim.lsp.buf.hover, '[P]eek [D]ocumentation')
-            nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
+            nmap('<leader>ps', vim.lsp.buf.signature_help, '[P]eek [S]ignature')
 
             -- Lesser used LSP functionality
             nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
@@ -52,11 +47,12 @@ return {
         require('which-key').register {
             ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
             ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-            ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
+            ['<leader>g'] = { name = '[G]rep', _ = 'which_key_ignore' },
             ['<leader>h'] = { name = '[H]arpoon', _ = 'which_key_ignore' },
             ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
             ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
             ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+            ['<leader>p'] = { name = '[P]eek', _ = 'which_key_ignore' },
         }
 
         -- mason-lspconfig requires that these setup functions are called in this order
