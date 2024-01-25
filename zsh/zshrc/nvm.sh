e0769ed502759f0dbff4fef560cf6266f23848be
Riley Mathews <rileymathews80@gmail.com>
2024-01-25 14:06:32 -0600

    better lazy load script

--- zsh/zshrc/nvm.sh
+++ zsh/zshrc/nvm.sh
@@ -1,29 +1,18 @@
-# This script should be sourced from .zprofile to take advantage of the $ZSH_EXECUTION_STRING check
 export NVM_DIR="$HOME/.nvm"
-export NVM_SYMLINK_CURRENT=true
-NODE_VERSION_DIR=$(readlink ~/.nvm/current) # Assumes ~/.nvm/current is linked
-__NODE_GLOBALS=("${(@f)$(find "$NODE_VERSION_DIR"/bin -maxdepth 1 -mindepth 1 -type l -print0 | xargs --null -n1 basename | sort --unique)}")
-__NODE_GLOBALS+=(node nvm)
+#[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+#[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-found=false
-for value in "${__NODE_GLOBALS[@]}"; do
-  # if zsh is trying to execute a command that matches one of the node globals in the current version
-  if [[ $ZSH_EXECUTION_STRING == *"$value"* ]]; then
-    # Forget about lazy loading and just add the current nvm node version to the PATH
-    PATH=~/.nvm/current/bin:$PATH
-    found=true
-    break
-  fi
+NODE_GLOBALS=(`find ~/.nvm/versions/node -maxdepth 3 -type l -wholename '*/bin/*' | xargs -n1 basename | sort | uniq`)
+NODE_GLOBALS+=(node nvm yarn)
+
+_load_nvm() {
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+}
+
+for cmd in "${NODE_GLOBALS[@]}"; do
+eval "function ${cmd}(){ unset -f ${NODE_GLOBALS[*]}; _load_nvm; unset -f _load_nvm; ${cmd} \$@; }"
 done
+unset cmd NODE_GLOBALS
 
-# instead of using --no-use flag, load nvm lazily:
-if ! $found; then
-  _load_nvm() {
-    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-  }
-
-  for cmd in "${__NODE_GLOBALS[@]}"; do
-    eval "function ${cmd}(){ unset -f ${__NODE_GLOBALS[*]}; _load_nvm; unset -f _load_nvm; ${cmd} \"\$@\"; }"
-  done
-fi
-unset cmd value found __NODE_GLOBALS
+export PATH="$PATH:$HOME/.yarn/bin"
