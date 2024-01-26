c73ccd7177e9874470923d9e0261fdfbda1e14b8
Riley Mathews <rileymathews80@gmail.com>
2024-01-26 12:00:34 -0600

    polished up install script

--- install.sh
+++ install.sh
@@ -1,13 +1,64 @@
 #! /usr/bin/env bash
 
-if [[ "$(uname)" == "Linux" ]]; then
+is_linux() {
+    if [[ "$(uname)" == "Linux" ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+is_mac() {
+    if [[ "$(uname)" == "Darwin" ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+command_installed() {
+    if command -v "$1" > /dev/null 2>&1; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+linux_install() {
+    if dpkg -l $1 > /dev/null 2>&1; then
+        echo "$1 already installed"
+    else
+        echo "installing $1"
+        sudo apt install $1 -y
+    fi
+}
+
+if is_linux; then
     echo "Linux OS detected..."
-    sudo apt install libevent-dev ncurses-dev build-essential bison pkg-config
-elif [[ "$(uname)" == "Darwin" ]]; then
+    linux_install "libevent-dev"
+    linux_install "ncurses-dev"
+    linux_install "build-essential"
+    linux_install "bison"
+    linux_install "pkg-config"
+    linux_install "libfreetype6-dev"
+    linux_install "libfontconfig1-dev"
+    linux_install "libxcb-xfixes0-dev"
+    linux_install "libxkbcommon-dev"
+    linux_install "python3"
+elif is_mac; then
     echo "MacOS detected..."
 else
     echo "could not determine OS. Exiting."
     exit 1
+fi
+
+if command_installed "rustup"; then
+    echo "rustup found, skipping installation"
+else
+    echo "installing rustup"
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+    rustup override set stable
+    rustup update stable
 fi
 
 NEOVIM_INSTALL_PATH="$HOME/neovim"
@@ -17,9 +68,15 @@ PYENV_INSTALL_PATH="$HOME/.pyenv"
 TPM_INSTALL_PATH="$HOME/.tmux/plugins/tpm"
 TMUX_INSTALL_PATH="$HOME/tmux"
 TMUX_SOURCE_PATH="$HOME/tmux-src"
+ALACRITTY_SOURCE_PATH="$HOME/alacritty-src"
+ALACRITTY_INSTALL_PATH="$HOME/alacritty"
 
 if [[ "$*" == *"--reinstall"* ]]; then
     echo "Reinstalling..."
+    echo "removing alacritty"
+    rm -rf $ALACRITTY_SOURCE_PATH
+    rm -rf $ALACRITTY_INSTALL_PATH
+
     echo "removing tmux"
     rm -rf $TMUX_INSTALL_PATH
     rm -rf $TMUX_SOURCE_PATH
@@ -34,6 +91,16 @@ if [[ "$*" == *"--reinstall"* ]]; then
     rm -rf $PYENV_INSTALL_PATH
     echo "removing tpm"
     rm -rf $TPM_INSTALL_PATH
+fi
+
+if [ -d "$ALACRITTY_INSTALL_PATH" ]; then
+    echo "alacritty found, not installing"
+else
+    git clone https://github.com/alacritty/alacritty $ALACRITTY_SOURCE_PATH
+    cd $ALACRITTY_SOURCE_PATH
+    cargo build --release
+    mkdir -p $ALACRITTY_INSTALL_PATH/bin
+    cp ./target/release/alacritty $ALACRITTY_INSTALL_PATH/bin
 fi
 
 if [ -d "$TMUX_INSTALL_PATH" ]; then
