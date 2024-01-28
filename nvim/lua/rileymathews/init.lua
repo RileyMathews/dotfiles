f7e0c5135c0e6f6af18508722786a3cc60265e9c
Riley Mathews <rileymathews80@gmail.com>
2024-01-27 22:33:01 -0600

    added mapping for html django projects

--- nvim/lua/rileymathews/init.lua
+++ nvim/lua/rileymathews/init.lua
@@ -1,6 +1,9 @@
 vim.filetype.add({
     extension = {
         tera = "htmldjango",
+    },
+    pattern = {
+        ["*/templates/**/*.html"] = "htmldjango"
     }
 })
 require("rileymathews.keybindings")
