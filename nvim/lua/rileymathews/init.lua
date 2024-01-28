0a9b9f2af6e5e84a91286787d652885d592f28e4
Riley Mathews <rileymathews80@gmail.com>
2024-01-27 22:41:11 -0600

    revert

--- nvim/lua/rileymathews/init.lua
+++ nvim/lua/rileymathews/init.lua
@@ -2,8 +2,5 @@ vim.filetype.add({
     extension = {
         tera = "htmldjango",
     },
-    pattern = {
-        ["*/templates/**/*.html"] = "htmldjango"
-    }
 })
 require("rileymathews.keybindings")
