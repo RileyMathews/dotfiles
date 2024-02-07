c518e58f1c36d1f5d5527902c3bfde8367db630b
Riley Mathews <rileymathews80@gmail.com>
2024-02-07 13:27:33 -0600

    keybindings

--- nvim/lua/rileymathews/keybindings.lua
+++ nvim/lua/rileymathews/keybindings.lua
@@ -1,6 +1,7 @@
 -- misc rebinds
 -- open neotree
-vim.keymap.set("n", "<leader>e", "<cmd>Neotree toggle<cr>", { desc = "[E]xplore files" })
+vim.keymap.set("n", "<leader>e", "<cmd>Neotree<cr>", { desc = "[E]xplore files" })
+vim.keymap.set("n", "<c-b>", "<cmd>Neotree toggle<cr>")
 
 -- move lines up and down
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
