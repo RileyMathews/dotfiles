9433b7b9d262b34d9fb4b8251c8b19ca24045d33
Riley Mathews <rileymathews80@gmail.com>
2024-03-05 10:13:29 -0600

    fixed themes

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,4 +1,12 @@
 return {
-	'nvim-telescope/telescope.nvim', tag = '0.1.4',
-	dependencies = { 'nvim-lua/plenary.nvim' },
+	"nvim-telescope/telescope.nvim",
+	tag = "0.1.4",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	opts = {
+		pickers = {
+			find_files = {
+				hidden = true,
+			},
+		},
+	},
 }
