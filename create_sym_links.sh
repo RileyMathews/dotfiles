d92b4d6dea39f1a2f9e7720c1a91a7dd88135856
Riley Mathews <rileymathews80@gmail.com>
2024-03-26 09:24:25 -0500

    updated desktop

--- create_sym_links.sh
+++ create_sym_links.sh
@@ -15,9 +15,10 @@ rm -rf ~/.config/i3
 rm -rf ~/.config/alacritty
 rm -rf ~/.config/starship.toml
 rm -rf ~/.Xresources
-rm -rf ~/.Xsessionrc
+rm -rf ~/.xsessionrc
 rm -rf ~/.config/rofi
 rm -rf ~/.config/aerospace
+rm -rf ~/.config/i3status/config
 
 if directory_present ~/.config; then
     echo "config directory already present"
@@ -33,6 +34,8 @@ ln -s `pwd`/zsh/.zshrc ~/.zshrc
 ln -s `pwd`/zsh/zshrc ~/zshrc
 mkdir -p ~/.config/i3
 ln -s `pwd`/i3/config ~/.config/i3/config
+mkdir -p ~/.config/i3status
+ln -s `pwd`/i3status/config ~/.config/i3status/config
 mkdir -p ~/.config/alacritty
 ln -s `pwd`/alacritty/alacritty.toml ~/.config/alacritty/alacritty.toml
 ln -s `pwd`/starship/starship.toml ~/.config/starship.toml
