09c5fa498b409a99653a37bcc86dd30db6ac3535
Riley Mathews <rileymathews80@gmail.com>
2024-04-04 14:41:24 -0500

    fixed

--- arch-install.sh
+++ arch-install.sh
@@ -12,6 +12,10 @@ sudo pacman -S --needed \
 	fzf \
 	ripgrep
 
+yay -S --needed \
+	rbenv
+
 LOCAL_SHARE_DIR="$HOME/.local/share"
 ZSH_SYNTAX_HIGHLIGTING_DIRECTORY=$LOCAL_SHARE_DIR/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_SYNTAX_HIGHLIGTING_DIRECTORY
+git clone https://github.com/nvm-sh/nvm ~/.nvm
