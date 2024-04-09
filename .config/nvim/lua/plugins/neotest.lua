6c83f4ef41807d9fa73a696443695ab46159a137
Riley Mathews <rileymathews80@gmail.com>
2024-04-09 15:40:43 -0500

    main -- neotest

---
+++ .config/nvim/lua/plugins/neotest.lua
@@ -0,0 +1,18 @@
+return {
+	"nvim-neotest/neotest",
+	dependencies = {
+		"nvim-neotest/nvim-nio",
+		"nvim-lua/plenary.nvim",
+		"antoinemadec/FixCursorHold.nvim",
+		"nvim-treesitter/nvim-treesitter",
+		"nvim-neotest/neotest-jest",
+	},
+	config = function()
+		local neotest = require("neotest")
+		neotest.setup({
+			adapters = {
+				require("neotest-jest")
+			}
+		})
+	end
+}
