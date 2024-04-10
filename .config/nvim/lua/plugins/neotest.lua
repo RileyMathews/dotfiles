6bde11fcbdef61decf8851bad86bc68c08870e49
Riley Mathews <rileymathews80@gmail.com>
2024-04-10 10:21:12 -0500

    main -- added neotest

--- .config/nvim/lua/plugins/neotest.lua
+++ .config/nvim/lua/plugins/neotest.lua
@@ -14,5 +14,20 @@ return {
 				require("neotest-jest")
 			}
 		})
+		vim.keymap.set("n", "<leader>tn", function()
+			neotest.run.run()
+		end, { desc = "[T]est [N]earest" })
+		vim.keymap.set("n", "<leader>to", neotest.output.open, { desc = "[T]est [O]pen output" })
+		vim.keymap.set("n", "<leader>tf", function()
+			neotest.run.run(vim.fn.expand("%"))
+		end, { desc = "[T]est [F]ile" })
+		vim.keymap.set("n", "<leader>td", function()
+			neotest.run.run({ strategy = "dap" })
+		end, { desc = "[T]est [D]ebug" })
+		vim.keymap.set("n", "<leader>ta", function()
+			neotest.run.run({ suite = true })
+		end, { desc = "[T]est [A]ll" })
+		vim.keymap.set("n", "<leader>tl", neotest.run.run_last, { desc = "[T]est [A]ll" })
+		vim.keymap.set("n", "<leader>ts", neotest.summary.toggle, { desc = "[T]est [S]ummary" })
 	end
 }
