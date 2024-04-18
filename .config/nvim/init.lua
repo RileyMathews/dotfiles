5f603376b45c36be93cb75dee447dbfbf18fddc9
Riley Mathews <rmathews@eventbrite.com>
2024-04-18 09:41:11 -0500

    main -- do not enter comments when making new line off existing comment

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -19,18 +19,18 @@ vim.opt.signcolumn = "yes"
 vim.opt.updatetime = 50
 
 vim.g.mapleader = " "
-
+vim.cmd("autocmd BufEnter * set formatoptions-=cro")
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 require("lazy").setup("plugins")
@@ -38,9 +38,9 @@ require("rileymathews")
 
 -- treesitter parser does not handle ruby very well
 -- this change is a bandaid that fixes an issue
--- where treesitter will outdent your code 
+-- where treesitter will outdent your code
 -- when typing a . on some occasions
 vim.api.nvim_create_autocmd("Filetype", {
-    pattern = "ruby",
-    command = "setlocal indentkeys-=."
+	pattern = "ruby",
+	command = "setlocal indentkeys-=.",
 })
