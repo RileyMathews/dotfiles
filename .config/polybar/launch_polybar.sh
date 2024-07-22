143e61756f002a8257a9e6e58916a68158564087
Riley Mathews <rileymathews80@gmail.com>
2024-07-22 14:08:30 -0500

    polybar updates

--- .config/polybar/launch_polybar.sh
+++ .config/polybar/launch_polybar.sh
@@ -1,9 +1,32 @@
-#! /bin/sh
+#!/bin/bash
+
 killall polybar
+
 if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload toph &> ~/.local/share/polybar/polybar.log &
-  done
+  # Check if there's a primary monitor
+  primary_monitor=$(xrandr --query | grep " connected primary" | cut -d" " -f1)
+  
+  if [ -n "$primary_monitor" ]; then
+    # If there's a primary monitor, launch the primary bar on it
+    MONITOR=$primary_monitor polybar --reload primary &> ~/.local/share/polybar/polybar_primary.log &
+    
+    # Launch the secondary bar on all other connected monitors
+    for m in $(xrandr --query | grep " connected" | grep -v "primary" | cut -d" " -f1); do
+      MONITOR=$m polybar --reload secondary &> ~/.local/share/polybar/polybar_secondary_${m}.log &
+    done
+  else
+    # If there's no primary monitor, get the first connected monitor
+    first_monitor=$(xrandr --query | grep " connected" | head -n 1 | cut -d" " -f1)
+    
+    # Launch the primary bar on the first monitor
+    MONITOR=$first_monitor polybar --reload primary &> ~/.local/share/polybar/polybar_primary.log &
+    
+    # Launch the secondary bar on all other connected monitors
+    for m in $(xrandr --query | grep " connected" | grep -v "$first_monitor" | cut -d" " -f1); do
+      MONITOR=$m polybar --reload secondary &> ~/.local/share/polybar/polybar_secondary_${m}.log &
+    done
+  fi
 else
-  polybar --reload toph &
+  # Fallback if xrandr is not available
+  polybar --reload primary &
 fi
