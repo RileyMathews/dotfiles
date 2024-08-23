ce1ba2fa3b6e82410ec32f40afe58d47dbeda90c
Riley Mathews <rileymathews80@gmail.com>
2024-08-22 21:32:46 -0500

    stylua working

--- flake.nix
+++ flake.nix
@@ -8,11 +8,13 @@
       system = "x86_64-linux";  # Change this if you're on a different architecture
       pkgs = import nixpkgs { inherit system; };
     in {
-      devShells.default = pkgs.mkShell {
-        buildInputs = [
-          pkgs.stylua
-          pkgs.lua-language-server
-        ];
+      devShells.${system} = {
+        default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.stylua
+            pkgs.lua-language-server
+          ];
+        };
       };
     };
 }
