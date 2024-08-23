aa9ed179ad368e7e64a9ab284b07257034d3703e
Riley Mathews <rileymathews80@gmail.com>
2024-08-22 21:18:44 -0500

    fixed completions

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -3,25 +3,27 @@ return { -- Autocompletion
 	event = "InsertEnter",
 	dependencies = {
 		-- Snippet Engine & its associated nvim-cmp source
-		{
-			"L3MON4D3/LuaSnip",
-			build = (function()
-				-- Build Step is needed for regex support in snippets
-				-- This step is not supported in many windows environments
-				-- Remove the below condition to re-enable on windows
-				if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
-					return
-				end
-				return "make install_jsregexp"
-			end)(),
-		},
-		"saadparwaiz1/cmp_luasnip",
+		-- {
+		-- 	"L3MON4D3/LuaSnip",
+		-- 	build = (function()
+		-- 		-- Build Step is needed for regex support in snippets
+		-- 		-- This step is not supported in many windows environments
+		-- 		-- Remove the below condition to re-enable on windows
+		-- 		if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
+		-- 			return
+		-- 		end
+		-- 		return "make install_jsregexp"
+		-- 	end)(),
+		-- },
+		-- "saadparwaiz1/cmp_luasnip",
 
 		-- Adds other completion capabilities.
 		--  nvim-cmp does not ship with all sources by default. They are split
 		--  into multiple repos for maintenance purposes.
 		"hrsh7th/cmp-nvim-lsp",
 		"hrsh7th/cmp-path",
+		"hrsh7th/cmp-buffer",
+		"kristijanhusak/vim-dadbod-completion",
 
 		-- If you want to add a bunch of pre-configured snippets,
 		--    you can use this plugin to help you. It even has snippets
@@ -31,16 +33,17 @@ return { -- Autocompletion
 	},
 	config = function()
 		-- See `:help cmp`
+		print("setting up cmp")
 		local cmp = require("cmp")
-		local luasnip = require("luasnip")
-		luasnip.config.setup({})
+		-- local luasnip = require("luasnip")
+		-- luasnip.config.setup({})
 
 		cmp.setup({
-			snippet = {
-				expand = function(args)
-					luasnip.lsp_expand(args.body)
-				end,
-			},
+			-- snippet = {
+			-- 	expand = function(args)
+			-- 		luasnip.lsp_expand(args.body)
+			-- 	end,
+			-- },
 			completion = { completeopt = "menu,menuone,noinsert" },
 
 			-- For an understanding of why these mappings were
@@ -71,28 +74,28 @@ return { -- Autocompletion
 				--
 				-- <c-l> will move you to the right of each of the expansion locations.
 				-- <c-h> is similar, except moving you backwards.
-				["<C-l>"] = cmp.mapping(function()
-					if luasnip.expand_or_locally_jumpable() then
-						luasnip.expand_or_jump()
-					end
-				end, { "i", "s" }),
-				["<C-h>"] = cmp.mapping(function()
-					if luasnip.locally_jumpable(-1) then
-						luasnip.jump(-1)
-					end
-				end, { "i", "s" }),
+				-- ["<C-l>"] = cmp.mapping(function()
+				-- 	if luasnip.expand_or_locally_jumpable() then
+				-- 		luasnip.expand_or_jump()
+				-- 	end
+				-- end, { "i", "s" }),
+				-- ["<C-h>"] = cmp.mapping(function()
+				-- 	if luasnip.locally_jumpable(-1) then
+				-- 		luasnip.jump(-1)
+				-- 	end
+				-- end, { "i", "s" }),
 			}),
 			sources = {
 				{ name = "nvim_lsp" },
-				{ name = "luasnip" },
 				{ name = "path" },
-			},
-		})
-		cmp.setup.filetype({ "sql" }, {
-			sources = {
-				{ name = "vim-dadbod-completion" },
 				{ name = "buffer" },
 			},
 		})
+		-- cmp.setup.filetype({ "sql" }, {
+		-- 	sources = {
+		-- 		{ name = "vim-dadbod-completion" },
+		-- 		{ name = "buffer" },
+		-- 	},
+		-- })
 	end,
 }
