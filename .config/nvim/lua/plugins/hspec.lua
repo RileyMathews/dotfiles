85efc7724b3e1afc97cb6fe864c8d837f7a03f27
Riley Mathews <rileymathews80@gmail.com>
2024-12-24 13:30:35 -0600

    nvim changes

--- .config/nvim/lua/plugins/hspec.lua
+++
@@ -1,8 +0,0 @@
-return {
-	dir = "~/code/ghciwatch-hspec-eval.nvim",
-	config = function()
-		local eval = require("ghciwatch-hspec-eval")
-
-		vim.keymap.set("n", "<leader>th", eval.toggle_hspec_comments, { desc = "Toggle Hspec comments" })
-	end
-}
