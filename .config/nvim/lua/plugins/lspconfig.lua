5e3c9b103a5c8ff6f98506ec1537d8310980e360
Riley Mathews <rileymathews80@gmail.com>
2025-01-09 16:24:26 -0600

    fixed lsp config

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -87,7 +87,7 @@ return {
 
 				-- Rename the variable under your cursor
 				--  Most Language Servers support renaming across files, etc.
-				map("<leader>lr", vim.lsp.buf.rename, "[R]ename")
+				map("<leader>lR", vim.lsp.buf.rename, "[R]ename")
 
 				-- Execute a code action, usually your cursor needs to be on top of an error
 				-- or a suggestion from your LSP for this to activate.
