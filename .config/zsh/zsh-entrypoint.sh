0333210021fc4b5a80193b621adae763bf600996
Riley Mathews <rileymathews80@gmail.com>
2025-02-03 10:54:43 -0600

    move device specific env vars to external file

--- .config/zsh/zsh-entrypoint.sh
+++ .config/zsh/zsh-entrypoint.sh
@@ -55,6 +55,8 @@ export PATH="$PATH:$HOME/.local/bin:$HOME/.local/scripts:$HOME/.screenlayout"
 export KEYTIMEOUT=1
 export BROWSER=librewolf
 
+[ -f "$HOME/.local/device-specific/environment.sh" ] && source "$HOME/.local/device-specific/environment.sh"
+
 
 #####################################
 # Aliases                           #
