ba3914687bc469102c3c59407752bfd4b7f55658
Riley Mathews <rileymathews80@gmail.com>
2025-03-05 12:53:13 -0600

    add copilot hotkey

--- .config/nvim/lua/plugins/copilot-chat.lua
+++ .config/nvim/lua/plugins/copilot-chat.lua
@@ -4,4 +4,15 @@ return {
 	opts = {
 		model = "claude-3.7-sonnet-thought",
 	},
+	keys = {
+		{
+			"<leader>cc",
+			function()
+				local actions = require("CopilotChat.actions")
+				require("CopilotChat.integrations.telescope").pick(actions.prompt_actions())
+			end,
+			desc = "CopilotChat",
+			mode = { "n", "v" },
+		},
+	},
 }
