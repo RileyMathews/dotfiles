69a593135662de2858f151cd9a2645e102d64556
Riley Mathews <rileymathews80@gmail.com>
2025-03-06 14:32:18 -0600

    better monitor script

--- .local/python-scripts/lib/hypr_lib.py
+++ .local/python-scripts/lib/hypr_lib.py
@@ -1,9 +1,20 @@
 from dataclasses import dataclass
-from dataclasses_json import dataclass_json, Undefined
+from dataclasses_json import DataClassJsonMixin, dataclass_json, Undefined
 
 
 @dataclass_json(undefined=Undefined.EXCLUDE)
 @dataclass
-class HyprlandMonitorsResponse:
+class HyprlandMonitorsResponse(DataClassJsonMixin):
     id: int
     name: str
+    x: int
+    y: int
+    width: int
+    height: int
+    scale: float
+
+    def keyword_restore_command(self) -> str:
+        return f"keyword monitor {self.name},{self.width}x{self.height},{self.x}x{self.y},{self.scale}"
+
+    def disable_command(self) -> str:
+        return f"keyword monitor {self.name},disable"
