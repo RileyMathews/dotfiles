a54ef2e424ec7d1207db7c17cd1bc1c52dab31ea
Riley Mathews <rileymathews80@gmail.com>
2025-03-06 13:25:15 -0600

    new hyprmon script

---
+++ .local/python-scripts/lib/hypr_lib.py
@@ -0,0 +1,9 @@
+from dataclasses import dataclass
+from dataclasses_json import dataclass_json, Undefined
+
+
+@dataclass_json(undefined=Undefined.EXCLUDE)
+@dataclass
+class HyprlandMonitorsResponse:
+    id: int
+    name: str
