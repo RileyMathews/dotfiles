a5eda6618fcfd766f1cbee562f7bb0bb083fe9f0
Riley Mathews <rileymathews80@gmail.com>
2025-03-28 09:21:33 -0500

    cleaned up some config

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -7,10 +7,6 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		"WhoIsSethDaniel/mason-tool-installer.nvim",
 
-		-- Useful status updates for LSP
-		-- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-		{ "j-hui/fidget.nvim", tag = "legacy", opts = {} },
-
 		-- Additional lua configuration, makes nvim stuff amazing!
 		"folke/neodev.nvim",
 	},
