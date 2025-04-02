6926c48a23ea709904acd158db9fac430e87b858
Riley Mathews <rileymathews80@gmail.com>
2025-04-02 15:41:50 -0500

    harpoon 2

--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -1,17 +1,21 @@
 return {
 	"ThePrimeagen/harpoon",
+	branch = "harpoon2",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 	},
 	config = function()
-		local mark = require("harpoon.mark")
-		local ui = require("harpoon.ui")
-
-		vim.keymap.set("n", "<leader>ha", mark.add_file, { desc = "[H]arpoon [A]dd file" })
-		vim.keymap.set("n", "<leader>ht", ui.toggle_quick_menu, { desc = "[H]arpoon [T]oggle" })
+		local harpoon = require("harpoon")
+		harpoon:setup()
+		vim.keymap.set("n", "<leader>ha", function()
+			harpoon:list():add()
+		end, { desc = "[H]arpoon [A]dd file" })
+		vim.keymap.set("n", "<leader>ht", function()
+			harpoon.ui:toggle_quick_menu(harpoon:list())
+		end, { desc = "[H]arpoon [T]oggle" })
 		for i = 1, 9 do
 			vim.keymap.set("n", "<leader>" .. i, function()
-				ui.nav_file(i)
+				harpoon:list():select(i)
 			end)
 		end
 	end,
