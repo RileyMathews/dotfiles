09058a2d39c02226ddaf4a8683bf5cf02d7de80d
Riley Mathews <rileymathews80@gmail.com>
2025-04-26 17:28:43 -0500

    edited nvim beta

--- .config/nvim_beta/init.lua
+++ .config/nvim_beta/init.lua
@@ -18,6 +18,9 @@ opt.relativenumber = true
 
 
 vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
 vim.opt.clipboard = "unnamedplus"
 
 vim.keymap.set("n", "<leader><leader>x", "<cmd>source %<CR>")
