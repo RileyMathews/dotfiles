28c9dba79f6bcdfe9e7920c690bcf25bc16c5c98
Riley Mathews <rileymathews80@gmail.com>
2025-05-02 13:26:58 -0500

    fix wezterm color scheme

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,5 +1,5 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
-config.color_scheme = "Tokyo Night Storm"
+config.color_scheme = "Tokyo Night (Gogh)"
 config.enable_tab_bar = false
 return config
