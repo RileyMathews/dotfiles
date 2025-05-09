86ee9b2963d49dc8c4f3d9beaba3a35e977d7e1f
Riley Mathews <rileymathews80@gmail.com>
2025-05-09 13:16:09 -0500

    fix wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -2,4 +2,6 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 config.color_scheme = "Tokyo Night (Gogh)"
 config.enable_tab_bar = false
+config.unicode_version = 14
+-- config.font = wezterm.font("Hack Nerd Font")
 return config
