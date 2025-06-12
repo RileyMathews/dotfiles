d357c59addb60ee8e9d0a1fc1c428c5e14d59041
Riley Mathews <rileymathews80@gmail.com>
2025-06-12 12:10:37 -0400

    add ability to source secrets file

--- .config/zsh/zsh-entrypoint.sh
+++ .config/zsh/zsh-entrypoint.sh
@@ -239,3 +239,5 @@ hyprlog() {
     echo "copying the last hyprland log to home dir as hyprland.log"
     cp /run/user/1000/hypr/$(command ls -t /run/user/1000/hypr/ | head -n 1)/hyprland.log ~/hyprland.log
 }
+
+[ -f "$HOME/.local/secrets" ] && source "$HOME/.local/secrets" # ghcup-env
