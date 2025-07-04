242af5b3e58ecd2dc14f93ffed27fd69a4b42a99
Riley Mathews <rileymathews80@gmail.com>
2025-07-04 09:44:25 -0500

    optionally enable ai plugins

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -1,22 +1,26 @@
-return {
-	"zbirenbaum/copilot.lua",
-	cmd = "Copilot",
-	event = "InsertEnter",
-	config = function()
-		require("copilot").setup({
-			suggestgion = {
-				enabled = true,
-				auto_trigger = false,
-				keymap = {
-					next = "<M-n>",
+if vim.env.NVIM_ENABLE_COPILOT == "true" then
+	return {
+		"zbirenbaum/copilot.lua",
+		cmd = "Copilot",
+		event = "InsertEnter",
+		config = function()
+			require("copilot").setup({
+				suggestgion = {
+					enabled = true,
+					auto_trigger = false,
+					keymap = {
+						next = "<M-n>",
+					},
 				},
-			},
-		})
+			})
 
-		local suggestion = require("copilot.suggestion")
-		vim.keymap.set("i", "<M-n>", suggestion.next)
-		vim.keymap.set("i", "<M-p>", suggestion.prev)
-		vim.keymap.set("i", "<M-y>", suggestion.accept)
-		vim.keymap.set("i", "<M-d>", suggestion.dismiss)
-	end,
-}
+			local suggestion = require("copilot.suggestion")
+			vim.keymap.set("i", "<M-n>", suggestion.next)
+			vim.keymap.set("i", "<M-p>", suggestion.prev)
+			vim.keymap.set("i", "<M-y>", suggestion.accept)
+			vim.keymap.set("i", "<M-d>", suggestion.dismiss)
+		end,
+	}
+else
+	return {}
+end
