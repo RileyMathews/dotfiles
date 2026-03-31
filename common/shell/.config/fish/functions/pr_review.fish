169b465d6607c9e83f975c4940c97b4bb2ab5950
Riley Mathews <rileymathews80@gmail.com>
2026-03-31 15:16:39 -0500

    add review agent

--- common/shell/.config/fish/functions/pr_review.fish
+++ common/shell/.config/fish/functions/pr_review.fish
@@ -16,7 +16,7 @@ function pr_review -a directory -a pr_number
     tmux new-session -d -s $session_name -c $directory
     tmux new-window -t $session_name
     tmux send-keys -t $session_name:1 "nvim '+lua require(\"ghlite\").open_pr($pr_number)'" C-m
-    tmux send-keys -t $session_name:2 "opencode" C-m
+    tmux send-keys -t $session_name:2 "opencode --agent review --prompt 'generate a summary for this PR'" C-m
     cd $cwd
     tmux attach -t $session_name
 end
