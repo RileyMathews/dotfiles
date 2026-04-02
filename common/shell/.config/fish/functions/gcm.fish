4f1b90f72b889e15455ec5d3753b020711472cfd
Riley Mathews <rileymathews80@gmail.com>
2026-04-02 10:12:11 -0500

    fix names

--- common/shell/.config/fish/functions/gcm.fish
+++ common/shell/.config/fish/functions/gcm.fish
@@ -1,3 +1,3 @@
-function gcb -a branch_name -d 'make a new branch with worktrunk and zellij'
+function gcm -a branch_name -d 'make a new branch with worktrunk and zellij'
     wt switch -c (git_main_branch) -x 'tmux-sessionizer {{ worktree_path }}'
 end
--- common/shell/.config/fish/functions/gco.fish
+++ common/shell/.config/fish/functions/gco.fish
@@ -1,3 +1,3 @@
-function gcb -a branch_name -d 'make a new branch with worktrunk and zellij'
+function gco -a branch_name -d 'make a new branch with worktrunk and zellij'
     wt switch -c $branch_name -x 'tmux-sessionizer {{ worktree_path }}'
 end
