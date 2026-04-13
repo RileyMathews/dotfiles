ab12bc65dfecd2c0537b6afd6b9b4c54506b0ba1
Riley Mathews <rileymathews80@gmail.com>
2026-04-13 12:33:57 -0500

    restore old git commands

--- common/shell/.config/fish/functions/gcm.fish
+++
@@ -1,3 +0,0 @@
-function gcm -a branch_name -d 'make a new branch with worktrunk and zellij'
-    wt switch -c (git_main_branch) -x 'tmux-sessionizer {{ worktree_path }}'
-end
--- common/shell/.config/fish/functions/wtb.fish
+++ common/shell/.config/fish/functions/wtb.fish
@@ -1,3 +1,3 @@
-function gcb -a branch_name -d 'make a new branch with worktrunk and zellij'
+function wtb -a branch_name -d 'make a new branch with worktrunk and zellij'
     wt switch --no-cd -c $branch_name -x 'tmux-sessionizer {{ worktree_path }}'
 end
