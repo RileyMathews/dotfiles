9c5e0bc853b6b55a0e9b5d4c13f3d776f7a1f34a
Riley Mathews <rileymathews80@gmail.com>
2026-04-14 13:46:01 -0500

    better review script

--- common/.config/fish/functions/pr_review.fish
+++ common/.config/fish/functions/pr_review.fish
@@ -1,22 +1,50 @@
 function pr_review -a directory -a pr_number
-    echo "args:"
-    echo $directory
-    echo $pr_number
-
     set -l cwd (pwd)
     cd $directory
-    git checkout (git_main_branch)
-    git fetch
-    git pull
 
-    gh pr checkout $pr_number
+    set -l branch_name (gh pr view $pr_number --json headRefName --jq .headRefName)
+    if test $status -ne 0
+        cd $cwd
+        return 1
+    end
 
-    set -l session_name "review_$pr_number"
+    wt switch --no-cd pr:$pr_number
+    if test $status -ne 0
+        cd $cwd
+        return 1
+    end
 
-    tmux new-session -d -s $session_name -c $directory
-    tmux new-window -t $session_name
-    tmux send-keys -t $session_name:1 "nvim '+lua require(\"ghlite\").open_pr($pr_number)'" C-m
-    tmux send-keys -t $session_name:2 "opencode --agent review --prompt 'generate a summary for this PR'" C-m
+    set -l worktree_path (wt list --format=json | jq -r --arg branch "$branch_name" '.[] | select(.branch == $branch and .path) | .path' | string collect)
+    if test $status -ne 0
+        cd $cwd
+        return 1
+    end
+
     cd $cwd
-    tmux attach -t $session_name
+
+    if test -z "$worktree_path"
+        echo "Could not resolve worktree path for PR $pr_number ($branch_name)"
+        return 1
+    end
+
+    set -l session_name (basename $worktree_path)
+    set -l created_session 0
+
+    if not tmux has-session -t $session_name 2>/dev/null
+        set created_session 1
+        tmux new-session -d -s $session_name -c $worktree_path -n shell
+    end
+
+    if test $created_session -eq 1
+        tmux new-window -t $session_name:1 -c $worktree_path
+        tmux new-window -t $session_name:2 -c $worktree_path
+        tmux send-keys -t $session_name:1 "nvim '+lua require(\"ghlite\").open_pr($pr_number)'" C-m
+        tmux send-keys -t $session_name:2 "opencode --agent review --prompt 'generate a summary for this PR'" C-m
+    end
+
+    if test -z "$TMUX"
+        tmux attach-session -t $session_name
+    else
+        tmux switch-client -t $session_name
+    end
 end
