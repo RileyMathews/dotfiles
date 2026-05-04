44bc77dab3a2c0dd48ba16409fc8173d422bbd2e
Riley Mathews <rileymathews80@gmail.com>
2026-05-04 00:50:44 -0500

    any zig

--- common/.config/fish/config.fish
+++ common/.config/fish/config.fish
@@ -43,6 +43,8 @@ alias l 'ls -al'
 alias tss 'sudo tailscale switch'
 alias oc 'opencode'
 
+alias zig 'anyzig'
+
 alias mpr 'python manage.py runserver'
 alias mpmm 'python manage.py makemigrations'
 alias mpm 'python manage.py migrate'
