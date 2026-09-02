af83589d4f64306b18af4b91098fd564ec36ded1
Riley Mathews <rileymathews80@gmail.com>
2026-09-02 10:13:18 -0500

    hyprland improvements

--- dot_config/exact_hypr/hyprland.lua
+++ dot_config/exact_hypr/hyprland.lua
@@ -134,7 +134,7 @@ end
 
 local terminal = "alacritty"
 local menu = "rofi -show drun"
-local browser = hostname() == "picard" and "google-chrome" or "helium-browser"
+local browser = hostname() == "picard" and "google-chrome-stable" or "helium-browser"
 
 -------------------
 ---- AUTOSTART ----
